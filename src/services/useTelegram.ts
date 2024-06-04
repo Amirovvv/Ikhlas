1cdf7932e847ef82463d42f491969298cea2f186
Khasan Amirov <58747052+Amirovvv@users.noreply.github.com>
2024-06-04 23:23:29 +0300

    initial of libraries

---
+++ src/services/useTelegram.ts
@@ -0,0 +1,15 @@
+import WebApp from '@twa-dev/sdk';
+
+interface useTelegramType {
+  ready: () => void;
+}
+
+export default function useTelegram(): useTelegramType {
+  function ready(): void {
+    WebApp.ready();
+  }
+
+  return {
+    ready
+  }
+}
